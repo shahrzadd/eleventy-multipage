dd992da1c8cf140f3fe22932843cf961035c51be
shahrzadd <shahrzadd@gmail.com>
2022-04-05 20:18:57 -0400

    Initial Commit

--- dist/js/index.js
+++
@@ -1,9 +0,0 @@
-const genres = ['crime', 'history', 'western'];
-
-let randomNum = Math.floor(Math.random() * genres.length);
-let randomIdx = Math.floor(Math.random() * genres * 20);
-
-const genre = genres[randomNum];
-
-document.getElementById(genre + randomNum).className = 'show';
-9;
